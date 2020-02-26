8b4d70372fae6bf305f66da2d2f4f43925ab2369
ABerry057 <alexander_berry@brown.edu>
2020-02-25 21:50:24 -0500

    finished dir organizer, need to deploy to GCP

--- src/dir_organizer.py
+++ src/dir_organizer.py
@@ -18,27 +18,25 @@ for split in ["train", "test"]:
     split_dir_path.mkdir(parents=True)
     # read key table
     key_df = pd.read_csv(f'{data_dir}/{split}.csv')
+    dir_pairs = 0
     # read through pq files
     for i in range(4):
-        # read current pq file
+        # read current pq file, already proccessed
         # current_pq = pd.read_parquet(f'{data_dir}/{split}_image_data_{i}.parquet')
         current_pq = pd.read_parquet("/home/alex/Desktop/deep_learning/midterm/bengali-char-recognition/data/train_toy.parquet")
-        print(current_pq.head())
-        # do magical preprocessing
+        # PERFORM PREPROCESSING?
         for row in current_pq.iterrows():
             ID = row[1]["image_id"]
             key_row = key_df[key_df.image_id == ID]
-            labels = (key_row["grapheme_root"],
-                      key_row["vowel_diacritic"],
-                      key_row["consonant_diacritic"])
-            im = row[1].drop("image_id").to_numpy()
+            labels = (key_row["grapheme_root"].values[0],
+                      key_row["vowel_diacritic"].values[0],
+                      key_row["consonant_diacritic"].values[0])
+            im = row[1].drop("image_id").to_numpy().reshape(137,236)
             # im = row[1].drop("image_id").to_numpy().reshape(127,97)
-            print(im)
-            print(im.shape)
-            break
-            cv2.imwrite(f'{split_dir}/{ID}.png', im)
+            cv2.imwrite(f'{split_dir}/{ID}.png', np.float32(im))
             with open(f'{split_dir}/{ID}.text', "w") as text_file:
                 print(f"{labels}", file=text_file)
+            dir_pairs += 1
             del im
             del labels
             del key_row
@@ -48,5 +46,6 @@ for split in ["train", "test"]:
     del key_df
     del split_dir_path
     del split_dir
+    print(f"Created {split} directory, containing {dir_pairs} image-label pairs")
     break
         
